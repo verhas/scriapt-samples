1c94d5613166eb3481d170ae11a262cbb9f9d9d0
verhas <Peter_Verhas@epam.com>
2013-08-31 19:33:50 +0200

    directory was renamed

--- helloworld/src/main/java/com/javax0/scriapt/sample/HelloWorld.java
+++ helloworld/src/main/java/com/javax0/scriapt/sample/HelloWorld.java
@@ -2,7 +2,7 @@ package com.javax0.scriapt.sample;
 
 import com.javax0.scriapt.CompileScript;
 
-@CompileScript(value="firstRun.js",engine="JavaScript")
-public class Sample1 {
+@CompileScript(value="compilescripts/helloworld.js")
+public class HelloWorld {
 
 }
